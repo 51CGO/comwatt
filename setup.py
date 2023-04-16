be84b337967c324f191ab062008f8cf171520166
Christophe Godart <christophe@armaghast.eu>
2023-04-16 22:29:47 +0200

    Change author email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Python client for Comwatt',
     license='MIT',
     author='Christophe Godart',
-    author_email='christophe@armaghast.eu',
+    author_email='51CGO@armaghast.eu',
     install_requires=['selenium'],
     url='https://github.com/51CGO/comwatt',
     keywords=['Comwatt', 'client'],
