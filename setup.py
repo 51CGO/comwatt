a5351a45fc2ced37dbe4ee6f5b3ac8d86debd52d
Christophe Godart <51cgo@lilo.org>
2024-05-10 17:04:34 +0200

    Change version to 0.7.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.6.0',
+    version='0.7.0',
     name='comwatt',
     description='Python client for Comwatt',
     long_description=open('README.md', 'r').read(),
