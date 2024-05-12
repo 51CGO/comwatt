006f3d1a46dff3c49419f7e6b285dee5ef52e762
Christophe Godart <51cgo@lilo.org>
2024-05-12 18:31:52 +0200

    Version 0.10.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.9.0',
+    version='0.10.0',
     name='comwatt',
     description='Python client for Comwatt',
     long_description=open('README.md', 'r').read(),
